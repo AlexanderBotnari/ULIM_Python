33ae21db93e6e90b0b3542b467a2fef1cb1604ed
AlexanderBotnari <sandubotnari99@gmail.com>
2022-10-26 22:28:27 +0300

    add tuple

--- Laborator_4/lab_4.py
+++ Laborator_4/lab_4.py
@@ -1,15 +1,15 @@
 import random
 
 
-def average(lista):
-    suma = sum(lista)
-    result = suma / len(lista)
+def average(col):
+    suma = sum(col)
+    result = suma / len(col)
     return result
 
 
-# Lista
+# /////////////////////////////////Lista/////////////////////
 # x = [1, 2, 3, 4]
-# print(average(lista=x))
+# print(average(col=x))
 
 # citire de la tastatură
 # input_string = input('Introduceti elementele listei : ')
@@ -18,16 +18,36 @@ def average(lista):
 # # Convertam elementele in tipul int
 # for i in range(len(user_list)):
 #     user_list[i] = int(user_list[i])
-# print(average(lista=user_list))
+# print(average(col=user_list))
 
 # atribuire directa
-# print(average(lista=[1, 2, 3, 4]))
+# print(average(col=[1, 2, 3, 4]))
 
 # atribuire random
-user_list = []
-for _ in range(0, 5):
-    n = random.randint(1, 10)
-    user_list.append(n)
+# user_list = []
+# for _ in range(0, 5):
+#     n = random.randint(1, 10)
+#     user_list.append(n)
+# print(user_list)
+# print(average(col=user_list))
 
-print(user_list)
-print(average(lista=user_list))
+# //////////////////////Tuple///////////////////////////////
+# user_tuple = (1, 2, 3, 4)
+# print(user_tuple)
+# print(average(col=user_tuple))
+
+# citire de la tastatură
+# user_input = input('Introduceti elementele : ')
+# user_tuple = tuple(int(el) for el in user_input.split(' '))
+# print('tuple: ', user_tuple)
+# print(average(col=user_tuple))
+
+# atribuire directa
+# print(average(col=(1, 2, 3, 4)))
+
+# atribuire random
+user_tuple = ()
+for _ in range(0, 5):
+    user_tuple += (random.randint(1, 10),)
+print(user_tuple)
+print(average(col=user_tuple))
