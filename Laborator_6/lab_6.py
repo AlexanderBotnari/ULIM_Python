44f95f7770b4576b2a860ce4872f23288e9ff42c
AlexanderBotnari <sandubotnari99@gmail.com>
2022-10-31 21:31:53 +0200

    lab_6 bug fixes

--- Laborator_6/lab_6.py
+++ Laborator_6/lab_6.py
@@ -1,12 +1,7 @@
-class Angajat:
-    nr = None
-    nume = None
-    prenume = None
-    functie = None
-    adresa = None
-    telefon = None
-    salariu = None
+from pprint import pprint
 
+
+class Angajat:
     def __init__(self, nr=None, nume=None, prenume=None, functie=None, adresa=None, telefon=None, salariu=None):
         self.nr = nr
         self.nume = nume
@@ -16,30 +11,44 @@ class Angajat:
         self.telefon = telefon
         self.salariu = salariu
 
-    def print_info(self):
-        if self.nr is None \
-                and self.nume is None \
-                and self.prenume is None:
-            print("Numele, prenumele si nr de ordine sunt obligatorii!")
-        else:
-            print(self.nr, self.nume, self.prenume, self.functie, self.adresa, self.telefon, self.salariu)
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 
 class Administratie(Angajat):
-    categorie_de_manager = None
-    nr_persoane_conducere = None
+    def __init__(self, nr, nume, prenume, functie, adresa, telefon, salariu,
+                 categorie_de_manager=None, nr_persoane_conducere=None):
+        super().__init__(nr=nr, nume=nume, prenume=prenume, functie=functie, adresa=adresa, telefon=telefon,
+                         salariu=salariu)
+        self.categorie_de_manager = categorie_de_manager
+        self.nr_persoane_conducere = nr_persoane_conducere
 
-    def __init__(self):
-        super().__init__()
-
-
-class Proprietar(Angajat):
-    tip_proprietar = None
-    procent_actiuni = None
-
-    def __init__(self):
-        super().__init__()
+    def __str__(self):
+        return super().__str__()
 
 
-proprietar = Proprietar()
-proprietar.print_info()
+class Proprietar(Administratie):
+    def __init__(self, nr, nume, prenume, functie, adresa, telefon, salariu, categorie_de_manager,
+                 nr_persoane_conducere, tip_proprietar=None, procent_actiuni=None):
+        super().__init__(nr, nume, prenume, functie, adresa, telefon, salariu, categorie_de_manager,
+                         nr_persoane_conducere)
+        self.tip_proprietar = tip_proprietar
+        self.procent_actiuni = procent_actiuni
+
+    def __str__(self):
+        return super().__str__()
+
+
+angajat = Angajat(nr=1, nume="Botnari", prenume="Felicia", functie="casier",
+                  adresa="Sucevita 36/1", telefon="068735689", salariu=6000)
+print(angajat)
+
+administrator = Administratie(nr=2, nume="Botnari", prenume="Giku", functie="administrator",
+                              adresa="Straseni,Panasesti", telefon="068567432", salariu=10000,
+                              categorie_de_manager="superior", nr_persoane_conducere=4)
+print(administrator)
+
+proprietar = Proprietar(nr=3, nume="Botnari", prenume="Alexandru", functie="programator",
+                        adresa="Sucevita 36", telefon="068327374", salariu=12000, categorie_de_manager="suprem",
+                        nr_persoane_conducere=30, tip_proprietar="Sef Institutie", procent_actiuni=90)
+print(proprietar)
